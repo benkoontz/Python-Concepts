8701ac57344fddabf2c6dfdc437c2b641d4be64d
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-12 20:31:34 -0700

    Create first.py

---
+++ Lambda-in-Python/first.py
@@ -0,0 +1,7 @@
+# Lambda is a small function that has no name
+# They don't have a name becuase they're used one time
+# Functions are used when you need to use code over and over again
+
+
+answer = lambda x: x*7 # pass in a number and multiply it by 7
+print(answer(5))
