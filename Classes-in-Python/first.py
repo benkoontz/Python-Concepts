670369802d035f8174c108e01526ac41e30c2e5a
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-12 20:38:08 -0700

    Create first.py

---
+++ Classes-in-Python/first.py
@@ -0,0 +1,23 @@
+# A class is used to group variable and functions together
+
+
+class Enemy:
+    life = 3
+
+    def attack(self):
+        print('ouch')
+        self.life -= 1
+
+    def checkLife(self):
+        if self.life <= 0:
+            print('enemy is dead')
+        else:
+            print(str(self.life) + " life left")
+
+enemy1 = Enemy()
+enemy2 = Enemy()
+
+enemy1.attack()
+enemy1.attack()
+enemy1.checkLife()
+enemy2.checkLife()
