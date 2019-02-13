955fb6a66366abcce4688995e5d6e40ec4351be4
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-12 20:20:09 -0700

    Create first.py

---
+++ Pyhton-GUI-organizing-your-layout/first.py
@@ -0,0 +1,22 @@
+# this program shows how to organize your GUI layout
+
+from tkinter import *
+
+root = Tk()  # create a blank window
+
+topFrame = Frame(root) # make invisble container
+topFrame.pack()
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+button1 = Button(topFrame, text="button 1", fg="red")
+button2 = Button(topFrame, text="button 2", fg="blue")
+button3 = Button(topFrame, text="button 3", fg="green")
+button4 = Button(bottomFrame, text="button 4", fg="purple")
+
+button1.pack(side = LEFT)
+button2.pack(side = LEFT)
+button3.pack(side = LEFT)
+button4.pack(side = BOTTOM)
+
+root.mainloop()
