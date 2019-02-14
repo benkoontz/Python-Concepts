46f044395573d4b029c0176cedbdb654f9df4b45
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-14 16:41:59 -0700

    Create first.py

---
+++ Strings/first.py
@@ -0,0 +1,10 @@
+user = "Tuna McFish"
+print(user[0]) # prints T
+print(user[-1]) #prints h
+print(user[-4]) # prints F
+print(user[2:6]) # prints na M
+print(len(user))
+
+#'' means where to begin and where to end
+tuna = 'I don\'t know what pizza to order'
+print(tuna)
