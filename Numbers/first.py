116b00a2569d269b361e8207859546a778639448
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-14 16:58:27 -0700

    Create first.py

---
+++ Numbers/first.py
@@ -0,0 +1,25 @@
+sum = 7 + 4
+print(sum) #prints 11
+
+div = 12/4
+print(div) # prints 3.0
+
+sum2 = 8 + 2 *10
+print(sum2) # prints 28
+
+sum3 = (8 + 2)*10
+print(sum3) #prints 100
+
+round_down = 18//4
+print(round_down) #prints 4 since its 4.5 and rounds down
+
+mod = 18%4
+print(mod) # prints 2
+
+pow = 5**3
+print(pow) # prints 125 (5^3)
+
+tuna = 5
+print(tuna) # prints 5
+
+print(tuna + 5) # prints 10
