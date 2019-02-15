42bde4cca104a90409ca5a1cb947525cd128c82f
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-14 17:06:16 -0700

    Create firs.py

---
+++ for loop/firs.py
@@ -0,0 +1,12 @@
+# for
+
+foods = ['bacon', 'tuna', 'salad', 'pizza']
+
+for f in foods: # prints all elements
+    print(f)
+    print(len(foods)) # prints 4
+
+
+for g in foods[:2]: # prints bacon and tuna
+    print(g)
+    print(len(foods[:2])) # prints 2
