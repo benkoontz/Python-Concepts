46166055e5b799acc9f26bad445075f16cc89ad2
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-14 17:01:54 -0700

    Create first.py

---
+++ if, elif, else/first.py
@@ -0,0 +1,12 @@
+# if, elif, else
+
+name = "hello"
+
+if name is "ben":
+    print("hey ben")
+elif name is "hello":
+    print("hey hello")
+elif name is "lucy":
+    print("lucy")
+else:
+    print ("none matched")
